9a7b46e7802bc2d354b421bf7f50347a90b5de86
Álan Crístoffer <acristoffers@startmail.com>
2023-07-09 10:18:29 +0200

    feat: extract function signature information

--- src/parsed_file.rs
+++ src/parsed_file.rs
@@ -4,6 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+use std::collections::HashMap;
 use std::path::Path;
 
 use crate::formatter::format;
@@ -11,11 +12,23 @@ use crate::utils::read_to_string;
 use anyhow::{anyhow, Context, Result};
 use log::{error, info};
 use lsp_types::Url;
-use tree_sitter::Tree;
+use tree_sitter::{Node, Tree};
+
+#[derive(Debug)]
+pub struct FileTypeFunction {
+    name: String,
+    argin: usize,
+    argout: usize,
+    vargin: bool,
+    vargout: bool,
+    argout_names: Vec<String>,
+    argin_names: Vec<String>,
+    vargin_names: Vec<String>,
+}
 
 #[derive(Debug)]
 pub enum FileType {
-    Function(String),
+    Function(FileTypeFunction),
     Class(String),
     MScript,
 }
@@ -127,6 +140,7 @@ impl ParsedFile {
 
     fn define_type(&mut self) -> Result<()> {
         if let Some(tree) = &self.tree {
+            let tree = tree.clone();
             let root = tree.root_node();
             let mut cursor = root.walk();
             if root.child_count() > 0 {
@@ -140,11 +154,7 @@ impl ParsedFile {
                             }
                         }
                         "function_definition" => {
-                            if let Some(name) = child.child_by_field_name("name") {
-                                FileType::Function(name.utf8_text(self.contents.as_bytes())?.into())
-                            } else {
-                                return Err(anyhow!("Could not find class name"));
-                            }
+                            FileType::Function(self.define_function_type(child)?)
                         }
                         _ => FileType::MScript,
                     }
@@ -155,5 +165,117 @@ impl ParsedFile {
             error!("File has no tree: {}", self.file.as_str());
             Err(anyhow!("File has no tree"))
         }
+    }
+
+    fn define_function_type(&mut self, node: Node) -> Result<FileTypeFunction> {
+        let name = if let Some(name) = node.child_by_field_name("name") {
+            name.utf8_text(self.contents.as_bytes())?.to_string()
+        } else {
+            return Err(anyhow!("Could not find class name"));
+        };
+        let mut cursor = node.walk();
+        let mut argout: usize = 0;
+        let mut vargout = false;
+        let mut argout_names = vec![];
+        if let Some(output) = node
+            .named_children(&mut cursor)
+            .find(|c| c.kind() == "function_output")
+        {
+            if let Some(args) = output.child(0) {
+                if args.kind() == "identifier" {
+                    argout = 1;
+                    argout_names.push(args.utf8_text(self.contents.as_bytes())?.into());
+                } else {
+                    argout = args.named_child_count();
+                    let mut cursor2 = args.walk();
+                    for arg_name in args
+                        .named_children(&mut cursor2)
+                        .filter(|c| c.kind() == "identifier")
+                        .filter_map(|c| c.utf8_text(self.contents.as_bytes()).ok())
+                        .map(String::from)
+                    {
+                        if arg_name == "varargout" {
+                            vargout = true;
+                        } else {
+                            argout_names.push(arg_name);
+                        }
+                    }
+                    if vargout {
+                        argout -= 1;
+                    }
+                }
+            }
+        }
+        let mut argin: usize = 0;
+        let mut vargin = false;
+        let mut argin_names = vec![];
+        let mut vargin_names = vec![];
+        if let Some(inputs) = node
+            .named_children(&mut cursor)
+            .find(|c| c.kind() == "function_arguments")
+        {
+            argin = inputs.named_child_count();
+            let mut cursor2 = node.walk();
+            let mut cursor3 = node.walk();
+            let mut cursor4 = node.walk();
+            for arg_name in inputs
+                .named_children(&mut cursor2)
+                .filter_map(|c| c.utf8_text(self.contents.as_bytes()).ok())
+                .map(String::from)
+            {
+                argin_names.push(arg_name);
+            }
+            let mut optional_arguments = HashMap::new();
+            for argument in node
+                .named_children(&mut cursor2)
+                .filter(|c| c.kind() == "arguments_statement")
+            {
+                if let Some(attributes) = argument
+                    .named_children(&mut cursor3)
+                    .find(|c| c.kind() == "attributes")
+                {
+                    if attributes
+                        .named_children(&mut cursor4)
+                        .filter_map(|c| c.utf8_text(self.contents.as_bytes()).ok())
+                        .any(|c| c == "Output")
+                    {
+                        continue;
+                    }
+                }
+                for property in argument
+                    .named_children(&mut cursor3)
+                    .filter_map(|c| c.child_by_field_name("name"))
+                    .filter(|c| c.kind() == "property_name")
+                {
+                    let arg_name = property
+                        .named_child(0)
+                        .unwrap()
+                        .utf8_text(self.contents.as_bytes())?
+                        .to_string();
+                    argin_names.retain(|e| *e != arg_name);
+                    optional_arguments.insert(arg_name, ());
+                    let opt_arg_name = property
+                        .named_child(1)
+                        .unwrap()
+                        .utf8_text(self.contents.as_bytes())?
+                        .to_string();
+                    vargin_names.push(opt_arg_name);
+                }
+            }
+            let vargin_count = optional_arguments.keys().count();
+            vargin = vargin_count > 0;
+            argin -= vargin_count;
+        }
+        let function = FileTypeFunction {
+            name,
+            argin,
+            argout,
+            vargin,
+            vargout,
+            argout_names,
+            argin_names,
+            vargin_names,
+        };
+        Ok(function)
     }
 }
