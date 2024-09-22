388636a5215a2014548fedb5a35461e58d0a5081
Álan Crístoffer <acristoffers@startmail.com>
2024-09-22 13:48:11 +0200

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -1,27 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         version = "1.0.0";
         pkgs = (import nixpkgs) { inherit system; };
-        naersk' = pkgs.callPackage naersk { };
+        nativeBuildInputs = with pkgs; [ cmake pkg-config rustc cargo ];
         buildInputs = [ ];
-        mkPackage = { name, buildInputs ? [ ] }: naersk'.buildPackage {
-          inherit buildInputs;
-          inherit name;
+        mkPackage = { name, buildInputs ? [ ] }: pkgs.rustPlatform.buildRustPackage {
+          pname = name;
           inherit version;
-          nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+          inherit buildInputs;
+          inherit nativeBuildInputs;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "tree-sitter-matlab-1.0.2" = "sha256-hueZg7hoZb11+ukrZgK1+da0w9J22jsj1BexlF4USXY=";
+              "matlab_beautifier-1.0.0" = "sha256-jJJ87BKYHvgsdLSUa19Wlk1xKa5vfYY8d7HESfcS5/E=";
+            };
+          };
           src = ./.;
           postInstall = "
-            cp -r target/release/share $out/share
+            cp -r target/*/release/share $out/share
           ";
         };
       in
